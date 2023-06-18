fe0149285ad1a98074337d11131228f2f2d125fb
ht38nhatphan <teonhat101@gmail.com>
2023-06-18 22:24:01 +0700

    update plusone

--- cod/leetcode/PlusOne.java
+++ cod/leetcode/PlusOne.java
@@ -1,5 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+    //   //solution clearn
+    //   for (int i = digits.length - 1; i >= 0; i--) {
+	  //       if (digits[i] < 9) {
+		//         digits[i]++;
+		//         return digits;
+	  //       }
+	  //       digits[i] = 0;
+    //   }
+
+    // digits = new int[digits.length + 1];
+    // digits[0] = 1;
+    // return digits;
         int len = digits.length;
         
         // int sum=digits[len-1];
