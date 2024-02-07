85cd20507ef97126e9416843e3ba36adc5a90c8f
ht38nhatphan <teonhat101@gmail.com>
2024-02-07 11:39:42 +0700

    fix

--- cod/leetcode/mySqrt.py
+++ cod/leetcode/mySqrt.py
@@ -1,17 +1,42 @@
-import math as m
+import math 
 class Solution(object):
-    def findnumber(self, s):
-        i,str= 0,""
-        while(i<=len(s) and s[i]!='.'):
-            str += s[i]
-            i+=1
-        
-        return str
     def mySqrt(self, x):
-        return (int(self.findnumber(str(m.sqrt(x)))))
-        
+        low = 1
+        high = x
 
+        if x == 0 or x == 1:
+            return x
+        
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
+        
+# #tham khảo       
+# import math 
+# class Solution(object):
+#     def mySqrt(self, x):
+#         low = 1
+#         high = x
+
+#         if x == 0 or x == 1:
+#             return x
+        
+#         while low <= high:
+#             mid = (low + high) // 2
+#             if mid * mid == x:
+#                 return mid
+#             elif mid * mid < x:
+#                 low = mid + 1
+#             else:
+#                 high = mid - 1
+#         return high
 if __name__ == '__main__':
     sl = Solution()
     
-    print(sl.mySqrt(6))
+    print(sl.mySqrt(8))
